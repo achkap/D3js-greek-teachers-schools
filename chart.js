3811c5d090cd4bb7331c0e30361b2d7ad9d2bd44
achilleas <kapetanios.achilleas@gmail.com>
2017-10-28 19:49:12 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -93,7 +93,7 @@ function start() {
 		.style("fill", function(d) { return fill(d.party); })
 		.on("mouseover", mouseover)
 		.on("mouseout", mouseout);
-		//.on("click", SearchGoogle);
+		.on("click", SearchGoogle);
 		//.on("click", function(d) { window.open("http://www.google.com/search?q=" + d.donor);}); //google search
 		// Alternative title based 'tooltips'
 		// node.append("title")
@@ -341,12 +341,12 @@ function mouseout() {
 			.style("display", "none");
 		}
 //google search
-/*function SearchGoogle()
+function SearchGoogle()
 {
     var query = d.donor;
     url ='http://www.google.com/search?q=' + query;
     window.open(url,'_blank');
-}*/
+}
 
 $(document).ready(function() {
 		d3.selectAll(".switch").on("click", function(d) {
