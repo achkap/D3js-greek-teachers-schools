e58462b09c5cb54384f3ec6b4020514870d23e8a
achilleas <kapetanios.achilleas@gmail.com>
2017-12-05 21:12:08 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -330,7 +330,7 @@ function mouseover(d, i) {
 		.html(infoBox)
 			.style("display","block");
 // Add text2speech
-  speech.text = donor + amount;
+  speech.text = donor + "for the" + amount;
   speech.volume = 1;
   speech.rate = 1;
   speech.pitch = 1;
