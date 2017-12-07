8ec2157133b2db51d550c5d0c921742794ef5d03
achilleas <kapetanios.achilleas@gmail.com>
2017-12-07 18:35:05 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -41,14 +41,13 @@ var tooltip = d3.select("#chart")
 
 var comma = d3.format(",.0f");
 
-function transition(name) {						// add bigdonor-type
+function transition(name) {
 	if (name === "all-donations") {
 		$("#initial-content").fadeIn(250);
 		$("#value-scale").fadeIn(1000);
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
-		//$("#view-bigdonor-type").fadeIn(250);
 		return total();
 		//location.reload();
 	}
@@ -57,7 +56,6 @@ function transition(name) {						// add bigdonor-type
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
-		//$("#view-bigdonor-type").fadeIn(250);
 		$("#view-party-type").fadeIn(1000);
 		return partyGroup();
 	}
@@ -66,29 +64,18 @@ function transition(name) {						// add bigdonor-type
 		$("#value-scale").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
-		//$("#view-bigdonor-type").fadeIn(250);
 		$("#view-donor-type").fadeIn(1000);
 		return donorType();
 	}
-	if (name === "group-by-money-source") {
+	if (name === "group-by-money-source")
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
-		//$("#view-bigdonor-type").fadeIn(250);
 		$("#view-source-type").fadeIn(1000);
 		return fundsType();
 	}
 
-	/*if (name === "group-by-big-donor") {
-		$("#initial-content").fadeOut(250);
-		$("#value-scale").fadeOut(250);
-		$("#view-donor-type").fadeOut(250);
-		$("#view-party-type").fadeOut(250);
-		$("#view-source-type").fadeIn(250);
-		$("#view-bigdonor-type").fadeIn(1000);
-		return bigdonorType();
-	}*/
 	
 function start() {
 
@@ -156,15 +143,6 @@ function fundsType() {
 		.on("tick", types)
 		.start();
 }
-	
-	// add big-donor function
-/*function bigdonorType() {
-	force.gravity(0)
-		.friction(0.75)
-		.charge(function(d) { return -Math.pow(d.radius, 2.0) / 3; })
-		.on("tick", bigdonors)
-		.start();
-}*/
 
 function parties(e) {
 	node.each(moveToParties(e.alpha));
@@ -188,16 +166,6 @@ function types(e) {
 			.attr("cy", function(d) {return d.y; });
 }
 
-	
-// add function bigdonors
-	
-/*function bigdonors(e) {
-	node.each(moveToBigdonor(e.alpha));
-	
-	node.attr("cx", function(d) { return d.x; })
-			.attr("cy", function(d) {return d.y; });
-}*/
-	
 function all(e) {
 	node.each(moveToCentre(e.alpha))
 		.each(collide(0.001));
@@ -274,24 +242,6 @@ function moveToFunds(alpha) {
 		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
 	};
 }
-	
-//add function moveTobigdonor
-/*function moveToBigdonor(alpha) {
-	return function(d) {
-		var centreY = entityCentres[d.entity].y;
-		var centreX = entityCentres[d.entity].x;
-		if (d.value <= 1000000) {
-			centreY = 300;
-			centreX = 350;
-		} else {
-			centreX = entityCentres[d.entity].x + 60;
-			centreY = 380;
-		}
-		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.1;
-		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
-	};
-}*/
- 
 
 // Collision detection function by m bostock
 function collide(alpha) {
@@ -361,7 +311,7 @@ function display(data) {
 
 function mouseover(d, i) {
 	// tooltip popup
-	var speech = new SpeechSynthesisUtterance();
+	var speech = new SpeechSynthesisUtterance();  //add speech variable 
 	var mosie = d3.select(this);
 	var amount = mosie.attr("amount");
 	var donor = d.donor;
@@ -381,17 +331,17 @@ function mouseover(d, i) {
 		.html(infoBox)
 			.style("display","block");
 // Add text2speech
-  speech.text = donor + "for the " + party + " party" + amount + "pounds";
+  speech.text = donor + " for the " + party + " party" + amount + "pounds";
   speech.volume = 1;
   speech.rate = 1;
   speech.pitch = 1;
 
   window.speechSynthesis.speak(speech);
-	}
+}
 
 function mouseout() {
 	// no more tooltips
-	var speech = new SpeechSynthesisUtterance();
+	var speech = new SpeechSynthesisUtterance(); //add speech variable
 		var mosie = d3.select(this);
 
 		mosie.classed("active", false);
@@ -418,5 +368,3 @@ $(document).ready(function() {
     return d3.csv("data/7500up.csv", display);
 
 });
-
-
